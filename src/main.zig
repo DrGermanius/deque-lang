db413215f59e23191333c994beb70c841c740548
DrGermanius <samolazov.herman@gmail.com>
2023-07-29 21:10:56 +0300

    simple number appending

--- src/main.zig
+++ src/main.zig
@@ -1,15 +1,22 @@
 const std = @import("std");
 const deque = @import("deque.zig");
 const print = std.debug.print;
+const data = @embedFile("./test.de");
 
 pub fn main() !void {
-    var a = try deque.Deque(u8).init();
-    try a.append(10, deque.Direction.Right);
-    try a.append(20, deque.Direction.Right);
-    try a.append(30, deque.Direction.Right);
-    try a.append(40, deque.Direction.Left);
-    try a.append(50, deque.Direction.Left);
-    try a.append(60, deque.Direction.Left);
-    try a.append(70, deque.Direction.Right);
-    a.printData();
+    var deq = try deque.Deque(u8).init();
+    var lines = std.mem.tokenize(u8, data, "\n");
+    while (lines.next()) |line| {
+        var it = std.mem.split(u8, line, " ");
+        while (it.next()) |x| {
+            if (x[0] == 33) {
+                try deq.append(try std.fmt.charToDigit(x[1], 10), .Left);
+            } else if (x[x.len - 1] == 33) {
+                try deq.append(try std.fmt.charToDigit(x[0], 10), .Right);
+            } else {
+                return;
+            }
+        }
+    }
+    deq.printData();
 }
